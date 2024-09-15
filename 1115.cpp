b2b7a73aa6db5930dc2acaf176c2eb0a57633646
ManishBachhu <manish.bachhu@nutanix.com>
2024-09-15 14:07:18 +0530

    Update 1115.cpp

--- 1115.cpp
+++ 1115.cpp
@@ -10,26 +10,22 @@ public:
         this->n = n;
     }
 
-    void foo(function<void()> printFoo) {
+    void run(function<bool()> condition, function<void()> printFunc) {
         for (int i = 0; i < n; i++) {
             unique_lock<mutex> lock(mtx);
-            cv.wait(lock, [this] {return foo_printed == false;});
+            cv.wait(lock, condition);
         	// printFoo() outputs "foo". Do not change or remove this line.
-        	printFoo();
-            foo_printed = true;
+        	printFunc();
+            foo_printed = (foo_printed)?false:true; // alternate between false and true
             cv.notify_all();
         }
     }
 
+    void foo(function<void()> printFoo) {
+        run([this] {return foo_printed == false;}, printFoo);
+    }
+
     void bar(function<void()> printBar) {
-        
-        for (int i = 0; i < n; i++) {
-            unique_lock<mutex> lock(mtx);
-            cv.wait(lock, [this] {return foo_printed == true;});
-        	// printBar() outputs "bar". Do not change or remove this line.
-        	printBar();
-            foo_printed = false;
-            cv.notify_all();
-        }
+        run([this] {return foo_printed == true;}, printBar);
     }
 };
