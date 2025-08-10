f3e48255731db6c0ecaf56bc1017591583f431e8
ManishBachhu <manish.bachhu@nutanix.com>
2025-08-10 22:02:40 +0530

    Queue Reconstruction by Height

---
+++ 406.cpp
@@ -0,0 +1,21 @@
+class Solution {
+    static bool comp(vector<int>& a, vector<int>& b) {
+        if(a[0] == b[0]) {
+            return a[1] < b[1];
+        }
+        return a[0] > b[0];
+    }
+
+public:
+    vector<vector<int>> reconstructQueue(vector<vector<int>>& people) {
+        sort(people.begin(), people.end(), comp);
+        vector<vector<int>> ans;
+
+        for(int i = 0; i < people.size(); i++) {
+            int k = people[i][1];
+            ans.insert(ans.begin() + k, people[i]);
+        }
+
+        return ans;
+    }
+};
