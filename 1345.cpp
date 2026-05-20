05d3929f448c7e7cac59e19e3767110b263177f6
ManishBachhu <manish.bachhu@nutanix.com>
2026-05-20 10:34:48 +0530

    Jump Game IV

--- 1345.cpp
+++ 1345.cpp
@@ -7,47 +7,46 @@ public:
         */
 
         int n = arr.size();
-        unordered_map<int,vector<int>> nodes;
+        unordered_map<int, vector<int>> val_to_index;
 
-        for(int i = 0; i < n; i++) {
-            nodes[arr[i]].push_back(i);
+        for (int i = 0; i < n; i++) {
+            val_to_index[arr[i]].push_back(i);
         }
 
         // Do BFS to get min path
         int target = n - 1, min_jumps = 0;
-        queue<int> node_q;
+        queue<int> bfs_q;
         vector<bool> visited(n);
-        node_q.push(0);
+        bfs_q.push(0);
         visited[0];
-        
-        while(!node_q.empty()) {
-            int size = node_q.size();
 
-            while(size--) {
-                int curr = node_q.front();
-                node_q.pop();
-                if(curr == target)
+        while (!bfs_q.empty()) {
+            int size = bfs_q.size();
+
+            while (size--) {
+                int curr = bfs_q.front();
+                bfs_q.pop();
+                if (curr == target)
                     return min_jumps;
-                
-                if(curr >= 1 && !visited[curr - 1]) {
-                    node_q.push(curr - 1);
+
+                if (curr >= 1 && !visited[curr - 1]) {
+                    bfs_q.push(curr - 1);
                     visited[curr - 1] = true;
                 }
 
-                if(curr < n - 1 && !visited[curr + 1]) {
-                    node_q.push(curr + 1);
+                if (curr < n - 1 && !visited[curr + 1]) {
+                    bfs_q.push(curr + 1);
                     visited[curr + 1] = true;
                 }
 
-
-                for(auto node: nodes[arr[curr]]) {
-                    if(!visited[node] && 
-                            (node < curr - 1 || node > curr + 1)) {
-                        node_q.push(node);
+                for (auto node : val_to_index[arr[curr]]) {
+                    if (!visited[node] &&
+                        (node < curr - 1 || node > curr + 1)) {
+                        bfs_q.push(node);
                         visited[node] = true;
                     }
                 }
-                nodes.erase(arr[curr]);
+                val_to_index.erase(arr[curr]);
             }
 
             min_jumps++;
